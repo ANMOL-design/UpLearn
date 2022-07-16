ac5c4e4405b33384110de1f0f6434f54b035cc47
ANMOL-design <65813590+ANMOL-design@users.noreply.github.com>
2022-07-16 14:06:43 +0530

    Navbar Done

--- frontend/src/components/Register/Register.jsx
+++ frontend/src/components/Register/Register.jsx
@@ -3,6 +3,8 @@ import React from "react";
 function Register(){
     return(
         <>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis illo accusamus dicta porro quia assumenda nisi, voluptatum tempore blanditiis est cumque ea fugiat tempora in beatae velit quasi alias repudiandae.</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis illo accusamus dicta porro quia assumenda nisi, voluptatum tempore blanditiis est cumque ea fugiat tempora in beatae velit quasi alias repudiandae.</p>
             <h1>Register User</h1>
         </>
     );
