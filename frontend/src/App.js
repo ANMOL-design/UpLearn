e161a68a8851e89efe032ab25dce91069eee1d43
ANMOL-design <65813590+ANMOL-design@users.noreply.github.com>
2022-07-18 19:07:34 +0530

    Navbar Complete and satisfy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,6 @@
 // Import a single style file only at index file
 import "./styles/main.scss"; 
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-=======
-import {Router, Routes, Route, BrowserRouter } from "react-router-dom";
->>>>>>> f1e2403dc542c891f06cbd11879962279d01b3c8
 // Importing  modules
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
@@ -13,7 +9,7 @@ import Register from "./components/Register/Register";
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <Router>
           {/* The Navbar component  */}         
           <Navbar />
           <Routes>
@@ -21,7 +17,7 @@ function App() {
             <Route path="/register" element={<Register />} />
           </Routes>
           {/* The Footer component  */}
-      </BrowserRouter>
+      </Router>
     </>
   );
 }
