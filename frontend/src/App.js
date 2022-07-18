fcfee8436efda6c66ddf1e4f3b3aae55116dd97e
ANMOL-design <65813590+ANMOL-design@users.noreply.github.com>
2022-07-19 00:24:28 +0530

    Created Footer pgf loader

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // Import a single style file only at index file
 import "./styles/main.scss"; 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 // Importing  modules
 import Navbar from "./components/Navbar/Navbar";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import Loader from "./components/Loader";
 
 function App() {
-  return (
+  const [isLoading, setisLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setisLoading(false);
+    }, 5000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+  else { return (
     <>
       <Router>
+          
           {/* The Navbar component  */}         
           <Navbar />
           <Routes>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="*" element={<PageNotFound />} />
           </Routes>
           {/* The Footer component  */}
+          <Footer />
       </Router>
     </>
   );
+  }
 }
 
 export default App;
---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import Logo from "./../../assets/images/Logo.png";
+import { Link } from "react-router-dom";
+import { FiArrowRight } from "react-icons/fi";
+import { BsTwitter, BsYoutube, BsFacebook } from "react-icons/bs";
+
+function Footer(){
+    return(
+        <>
+            <div className="foo">
+                <div className="foo-container">
+                    {/* The container 1 contain site Link  */}
+                    <div className="foo-brand">
+                        <div>
+                            <img src={Logo} alt="Logo" className="logo_ig" /> 
+                            <h1>UpLearn</h1>
+                        </div>
+                        <p>Great lesson ideas and lesson plans for ESL teachers! 
+                            Educators can customize lesson plans to best.
+                        </p>
+                        <span>
+                            <a href="google.com" rel="nofollow" to="_blank"><BsFacebook /></a>
+                            <a href="google.com" rel="nofollow" to="_blank"><BsTwitter /></a> 
+                            <a href="google.com" rel="nofollow" to="_blank"><BsYoutube /></a> 
+                        </span>
+                    </div>
+                    {/* The container 2 contain site Link  */}
+                    <div className="foo-feature">
+                        <h2>Features</h2>
+                        <Link to="/">About</Link>
+                        <Link to="/">Courses</Link>
+                        <Link to="/">Events</Link>
+                        <Link to="/">Instructor</Link>
+                        <Link to="/">Career</Link>
+                        <Link to="/">Become a Teacher</Link>
+                        <Link to="/">Contact</Link>
+                    </div>
+                    {/* The container 3 contain site Link  */}
+                    <div  className="foo-platform">
+                        <h2>Platform</h2>
+                        <Link to="/">Ask Doubts</Link>
+                        <Link to="/">Library</Link>
+                        <Link to="/">Courses</Link>
+                        <Link to="/">News &amp; Blogs</Link>
+                        <Link to="/">FAQs</Link>
+                        <Link to="/">Tutorials</Link>
+                    </div>
+                    {/* The container 3 contain site Link  */}
+                    <div  className="foo-Subscribe">
+                        <h2>Subscribe</h2>
+                        <div>
+                            <input type="text" name="subscribe" id="subscribe" placeholder="Your email address"/>
+                            <span><FiArrowRight /></span>
+                        </div>
+                        <span>Get the latest news and updates right at your inbox.</span>
+                    </div>
+                </div>
+            </div>
+            {/* Footer Bottom  */}
+            <div className="foo-bottom">
+                <p>© 2022 UpLearn, All Rights Reserved. Design By JM_ClashWithCode</p>
+            </div>
+        </>
+    )
+}
+
+export default Footer;
