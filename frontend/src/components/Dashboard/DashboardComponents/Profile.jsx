200843a99ed39bb4edc359d55962c65d58fc706b
ANMOL-design <65813590+ANMOL-design@users.noreply.github.com>
2022-07-25 11:43:00 +0530

    Dashboard Updated

--- frontend/src/components/Dashboard/DashboardComponents/Profile.jsx
+++ frontend/src/components/Dashboard/DashboardComponents/Profile.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Profile() {
-  return <div>Profile</div>;
+  return(
+    <div className="mainDashContainer">
+      {/* My Profile Content  */}
+      <h2>&nbsp;&nbsp;My Profile</h2>
+      <p>Set your bio, and other public-facing information.</p>
+    </div>
+  );
 }
