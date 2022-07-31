cbc582c25d057b79943d9a21ef9760703d7db6e1
ANMOL-design <65813590+ANMOL-design@users.noreply.github.com>
2022-07-31 19:36:00 +0530

    add home

--- frontend/src/components/Library/LibraryHome.jsx
+++ frontend/src/components/Library/LibraryHome.jsx
@@ -1,31 +1,42 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { MdSearch,MdLibraryAdd } from "react-icons/md";
+import { MdSearch } from "react-icons/md";
 import LibraryHome from "./LibraryBooks";
+import LibraryBanner from "./../../assets/images/Librarybanner.jpeg"
+import axios from "axios";
+var CryptoJS = require("crypto-js");
+
+
 export default function LibraryPage() {
+
   const loginDetails = useSelector((state) => state.userReducers);
   let navigate = useNavigate();
 
+  const [User,SetUser] = useState({});
+
   const [Library, SetLibrary] = useState([]);
-  const [BOOKS, setBOOks] = useState({
+  const [books, setbooks] = useState({
     bookCategory: "",
   });
+
   const handleChange = (event) => {
-    setBOOks({ ...BOOKS, [event.target.name]: event.target.value });
+    setbooks({ ...books, [event.target.name]: event.target.value });
   };
+
+  // Function to Present the Extra Slect option to filter out Data 
   const BOOKCHOICE = () => {
-    if (BOOKS.bookCategory === "School") {
+    if (books.bookCategory === "School") {
       return (
         <>
           <label htmlFor="bookclass">
             Select Class<span className="star">*</span>
           </label>
-          <select id="bookclass" name="bookclass">
-            <option disabled value="">
-              Select class:
-            </option>
+          <select id="bookclass" name="bookclass"  
+            value={books.bookclass}
+            onChange={(e) => handleChange(e)}
+          >
+            <option value="">Select class</option>
             <option value="1">Class 1</option>
             <option value="2">Class 2</option>
             <option value="3">Class 3</option>
@@ -41,27 +52,24 @@ export default function LibraryPage() {
           </select>
         </>
       );
-    } else if (BOOKS.bookCategory === "Exam") {
+    } 
+    else if (books.bookCategory === "Exam") {
       return (
         <>
           <label htmlFor="bookclass1">
             Select Exam<span className="star">*</span>
           </label>
-          <select
-            id="bookclass1"
-            name="bookclass"
-            value={BOOKS.bookclass}
+          <select id="bookclass1" name="bookclass"
+            value={books.bookclass}
             onChange={(e) => handleChange(e)}
           >
-            <option value="" disabled>
-              Select Exam:
-            </option>
-            <option value="Jee">Jee</option>
-            <option value="Neet">Neet</option>
-            <option value="Cat">Cat</option>
-            <option value="Gate">Gate</option>
-            <option value="Upsc">Upsc</option>
-            <option value="Other">Other</option>
+            <option value="">Select Exam</option>
+            <option value="JEE">Jee</option>
+            <option value="NEET">Neet</option>
+            <option value="CAT">Cat</option>
+            <option value="GATE">Gate</option>
+            <option value="UPSC">Upsc</option>
+            <option value="OTHER">Other</option>
           </select>
         </>
       );
@@ -70,41 +78,67 @@ export default function LibraryPage() {
 
   useEffect(() => {
     window.scroll(0, 120);
-    // Check is  Login Or Not
-    if (Number(loginDetails.isLoggedIn)) {
-      // call the fetch admin detail function
-      const fetchdata = async () => {
-        await axios
-          .get("/librarybooks")
-          .then((response) => {
-            SetLibrary(response.data);
+    // Decrypting the User Role
+    if(loginDetails.userRole !== ''){
+      var bytes = CryptoJS.AES.decrypt(loginDetails.userRole, 'my-secret-key@123');
+      var role = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+    }
+    // Check is  Login Or Not 
+    if (Number(loginDetails.isLoggedIn) && role === "INSTRUCTOR") 
+    {
+      // call the fetch admin detail function 
+      const fetchdata = async () =>{
+        await axios.get("/aboutInstructor").then(response => {
+          SetUser(response.data);
           })
-          .catch((error) => {
+          .catch(error => {
             console.log(error);
             navigate("/login");
           });
-      };
+      }
       fetchdata();
     }
-    // If User is not login redirect to login
-    else {
+    else if((Number(loginDetails.isLoggedIn) && role === "STUDENT")){
+        // call the fetch admin detail function 
+        const fetchdata = async () =>{
+            await axios.get("/aboutStudents").then(response => {
+              SetUser(response.data);
+              })
+              .catch(error => {
+                console.log(error);
+                navigate("/login");
+              });
+        }
+        fetchdata();
+    }
+    // If User is not login redirect to login 
+    else{
       navigate("/login");
     }
   }, [loginDetails.isLoggedIn]);
+
+  // Starting Main Component 
   return (
     <>
     <div className="lib-main-container">
-      <div className="Library-banner"></div>
+      {/* Banner Image of Library  */}
+      <div className="Library-banner">
+        <img src={LibraryBanner} alt="Banner" />
+      </div>
+      {/* Main Content of Library  */}
       <div className="library-container">
+        {/* Heading Library  */}
         <div className="library-heading">
           <h1>Uplearn Online Library</h1>
           <p>
-            Here you get all Ncert books also Books for preparing Exams Like
-            Jee,Neet,Cat,Upsc etc.
+            Here you get all NCERT books also Books for preparing Exams Like
+            JEE,NEET,CAT,UPSC etc.
           </p>
         </div>
+        {/* filter Out Function  */}
         <div className="library-filter-container">
           <div className="library-search-bar">
+            {/* Input Bar  */}
             <input list="library-search" name="librarySearch" />
             <datalist id="library-search">
               {Library.map((item) => (
@@ -114,9 +148,12 @@ export default function LibraryPage() {
             <button type="submit">
               <MdSearch />
             </button>
+
+            {/* Choices  */}
             <label htmlFor="bookCategory">
               Choose <span className="star">*</span>
-            </label>{" "}
+            </label>
+
             <input
               type="radio"
               id="School"
@@ -125,6 +162,7 @@ export default function LibraryPage() {
               onChange={(e) => handleChange(e)}
             />
             <label htmlFor="School"> For School</label>
+
             <input
               type="radio"
               id="Exam"
@@ -133,16 +171,18 @@ export default function LibraryPage() {
               onChange={(e) => handleChange(e)}
             />
             <label htmlFor="Exam"> For Competetive Exam</label>
+
             <BOOKCHOICE />
+            {/* Button to Filter  */}
             <button>Filter</button>
           </div>
         </div>
-
+        {/* Section Show all AvailableBooks  */}
         <div className="library-card-containerr">
             <LibraryHome/>
         </div>
       </div>
-      </div>
+    </div>
     </>
   );
 }
