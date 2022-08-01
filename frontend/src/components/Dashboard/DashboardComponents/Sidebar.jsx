aa9606ec73ff7ad6de5cd2c506c2745fa6d43397
Aastha <aastha252001@gmail.com>
2022-08-01 18:39:09 +0530

    modified style of sidebar

--- frontend/src/components/Dashboard/DashboardComponents/Sidebar.jsx
+++ frontend/src/components/Dashboard/DashboardComponents/Sidebar.jsx
@@ -43,7 +43,13 @@ const Sidebar = () => {
   ];
   return (
     <div className="container">
-      <div style={{ width: isOpen ? "200px" : "50px" }} className="sidebar">
+      <div
+       
+       
+      
+        style={{ width: isOpen ? "200px" : "50px" }}
+        className="stud_sidebar "
+      >
         <div className="top_section">
           <h1 style={{ display: isOpen ? "block" : "none" }} className="logo">
             UpLearn
