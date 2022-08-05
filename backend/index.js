3828c81ec22fed2b63ed3d3da9c54ccb29567fca
abhishekonweb <abhisheksharma3403@gmail.com>
2022-08-05 21:17:30 +0530

    m

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const PORT = 5000;
+const PORT = 4500;
 const dotenv = require("dotenv");
 dotenv.config();
 
