7d5fc79fa101372e95aa5ea69dd09045c9fff3dc
ANMOL-design <anmol1219216@jmit.ac.in>
2022-08-23 11:45:07 +0530

    sidebar

--- frontend/src/components/instructorDashboard/InstructorProfile/InstrutorComponent/MyClasses/ManageClass.jsx
+++ frontend/src/components/instructorDashboard/InstructorProfile/InstrutorComponent/MyClasses/ManageClass.jsx
@@ -1,20 +1,21 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { BiArrowBack } from "react-icons/bi";
-import { MdPadding, MdSearch } from "react-icons/md";
-import { Link, useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Loader from "../../../../Loader";
 import LiveClassAttendance from "./ClassManage/LiveClassAttendance";
 import SchedulseClass from "./ClassManage/ScheduleClass";
+
 export default function ManageClass() {
   const [MyClassroom, setMyClassroom] = useState({});
-  const [StudentInfo,  setStudentsInfo] = useState([]);
-  const [instructorInfo,  setinstructor] = useState([]);
+  const [StudentInfo, setStudentsInfo] = useState([]);
+  const [instructorInfo, setinstructor] = useState([]);
+
   const { id } = useParams();
   let navigate = useNavigate();
   const [Loading, setLoading] = useState(true);
+
   useEffect(() => {
-    window.scroll(0,0);
+    window.scroll(0, 0);
     const fetchClassroom = async () => {
       await axios
         .get("/myClass/" + id)
@@ -27,14 +28,16 @@ export default function ManageClass() {
         });
     };
     fetchClassroom();
-    const fetchdata = async () =>{
-        await axios.get("/allStudents").then(response => {
-            setStudentsInfo(response.data);
+    const fetchdata = async () => {
+      await axios
+        .get("/allStudents")
+        .then((response) => {
+          setStudentsInfo(response.data);
         })
-        .catch(error => {
-            console.log(error);
+        .catch((error) => {
+          console.log(error);
         });
-      }
+    };
     fetchdata();
     const fetchInstructor = async () => {
       await axios
@@ -49,8 +52,9 @@ export default function ManageClass() {
         });
     };
     fetchInstructor();
-  }, [id,Loading]);
-console.log(MyClassroom);
+  }, [id, Loading]);
+
+  // console.log(MyClassroom);
   // States to handle component changes in page
   const [ParticipantShow, setParticipantShow] = useState(true);
   const [NoticeShow, setNoticeShow] = useState(false);
@@ -98,50 +102,47 @@ console.log(MyClassroom);
   };
 
   const AddParticipants = () => {
-    const [err,seterr]= useState("");
-    const [newParticipant,setParticipant]= useState("");
-    const AddNewParticipant = async ()=>{
-        const isSameStudent = StudentInfo.find((i)=>i.email==newParticipant);
-          if(newParticipant===""){
-            seterr("Select Participant from the List")
-          }
-          else{
-             const isParticipantAdded = MyClassroom[0].classUsers.find((j)=>j==isSameStudent._id)
+    const [err, seterr] = useState("");
+    const [newParticipant, setParticipant] = useState("");
+    const AddNewParticipant = async () => {
+      const isSameStudent = StudentInfo.find((i) => i.email == newParticipant);
+      if (newParticipant === "") {
+        seterr("Select Participant from the List");
+      } else {
+        const isParticipantAdded = MyClassroom[0].classUsers.find(
+          (j) => j == isSameStudent._id
+        );
 
-             if(isParticipantAdded){
-                 seterr("Participant Already Added")
-             }
-             else{
-                const UserId = isSameStudent._id;
-                const classId = id;
-                const InstructorId = MyClassroom[0].classOwner
-                const res = await fetch("/Add-Participant", {
-                    method: "POST",
-                    headers: {
-                      "content-Type": "application/json",
-                    },
-                    body: JSON.stringify({
-                     UserId,
-                     classId,
-                     InstructorId
-                    }),
-                  });
-              
-                  if (res.status === 200) {
-                    seterr("Participant Added Succesfully")
-                    setLoading(true);
-                    setTimeout(() => {
-                      setLoading(false);
-                  
-                    },5000);
-                  } else {
-                    
-                    seterr("Something Went Wrong, Try Later\nError Occured");
-                  }
-                
-             }
+        if (isParticipantAdded) {
+          seterr("Participant Already Added");
+        } else {
+          const UserId = isSameStudent._id;
+          const classId = id;
+          const InstructorId = MyClassroom[0].classOwner;
+          const res = await fetch("/Add-Participant", {
+            method: "POST",
+            headers: {
+              "content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              UserId,
+              classId,
+              InstructorId,
+            }),
+          });
+
+          if (res.status === 200) {
+            seterr("Participant Added Succesfully");
+            setLoading(true);
+            setTimeout(() => {
+              setLoading(false);
+            }, 5000);
+          } else {
+            seterr("Something Went Wrong, Try Later\nError Occured");
           }
-    }
+        }
+      }
+    };
     if (MyClassroom[0].classUsers.length < 1) {
       return (
         <>
@@ -154,24 +155,22 @@ console.log(MyClassroom);
                     name="librarySearch"
                     value={newParticipant}
                     placeholder="What are you looking for ?"
-                    onChange={(e)=>setParticipant(e.target.value)}
+                    onChange={(e) => setParticipant(e.target.value)}
                   />
-                  {
-                      (StudentInfo.length > 0) ?
-                        <datalist id="library-search">
-                          {StudentInfo.map((item) => (
-                            <option value={item.email} />
-                          ))}
-                        </datalist>
-                      : null
-                  }
+                  {StudentInfo.length > 0 ? (
+                    <datalist id="library-search">
+                      {StudentInfo.map((item) => (
+                        <option value={item.email} />
+                      ))}
+                    </datalist>
+                  ) : null}
 
                   <button onClick={AddNewParticipant} type="submit">
                     Add Participant
                   </button>
                 </div>
                 <br />
-                  <p className="star">{err}</p>
+                <p className="star">{err}</p>
                 <br />
                 <h2 style={{ textAlign: "center" }}>
                   No Participants in class{" "}
@@ -182,30 +181,28 @@ console.log(MyClassroom);
         </>
       );
     } else {
-        let x=0;
-        let MyStudent;
+      let x = 0;
+      let MyStudent;
       return (
         <>
           <div className="add-participants-container">
             <div className="add-user-Container">
-            <div className="library-filter-container">
+              <div className="library-filter-container">
                 <div className="librarySearch">
                   <input
                     list="library-search"
                     name="librarySearch"
                     value={newParticipant}
-                    placeholder="Enter User Email to Search ..." 
-                    onChange={(e)=>setParticipant(e.target.value)}
+                    placeholder="Enter User Email to Search ..."
+                    onChange={(e) => setParticipant(e.target.value)}
                   />
-                  {
-                      (StudentInfo.length > 0) ?
-                        <datalist id="library-search">
-                          {StudentInfo.map((item) => (
-                            <option value={item.email} />
-                          ))}
-                        </datalist>
-                      : null
-                  }
+                  {StudentInfo.length > 0 ? (
+                    <datalist id="library-search">
+                      {StudentInfo.map((item) => (
+                        <option value={item.email} />
+                      ))}
+                    </datalist>
+                  ) : null}
 
                   <button onClick={AddNewParticipant} type="submit">
                     Add Participant
@@ -213,76 +210,87 @@ console.log(MyClassroom);
                 </div>
                 <br />
                 <p className="star">{err}</p>
-           
-        
-        {/* Table Show the Details of Teacher  */}
-        <div className="instructor-table" style={{marginTop:"40px"}}>
-         <p><b>Total Students: {MyClassroom[0].classUsers.length}</b></p>
-          <table>
-            <thead>
-              <tr>
-                <th>Sr. No</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Remove</th>
-              </tr>
-            </thead>
-            <tbody>
-                  {MyClassroom[0].classUsers.map((items)=>{
-                    let MyStudent = StudentInfo.find((i)=>i._id ===items)
-                    if(MyStudent){
-                    return (
-                        <>
-                      <tr key={MyStudent._id}>
-                        <td>
-                          {++x}
-                        </td>
-                        <td style={{textAlign: 'left', paddingLeft: '1rem'}}>
-                          {MyStudent.name}
-                        </td>
-                        <td style={{textTransform: 'none'}}>
-                          {MyStudent.email}
-                        </td>
-                        <td>
-                          <button className='rmvbtn' onClick={ async ()=>{
-                              const UserId = MyStudent._id;
-                              const ClassId = id;
-                              console.log(UserId)
-                              console.log(ClassId)
-                              const res =  await fetch("/removefromclass" ,{
-                                method : "POST",
-                                headers : { 
-                                    "content-Type" : "application/json"
-                                },
-                                body : JSON.stringify({
-                                  UserId,ClassId
-                                })
-                              });
-                              if (res.status === 200) {
-                                setLoading(true);
-                                setTimeout(() => {
-                                  setLoading(false);
-                              
-                                },5000);
-                              }
-                              else{
-                                console.log(res);
-                              }
-                            } 
-                          }
-                          >
-                            Delete
-                          </button>
-                        </td>
-                  </tr>
-                  </>
-                )}})}
-            </tbody>
-          </table>
-        </div>
-           </div>
+
+                {/* Table Show the Details of Teacher  */}
+                <div className="instructor-table" style={{ marginTop: "40px" }}>
+                  <p>
+                    <b>Total Students: {MyClassroom[0].classUsers.length}</b>
+                  </p>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Sr. No</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Remove</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {MyClassroom[0].classUsers.map((items) => {
+                        let MyStudent = StudentInfo.find(
+                          (i) => i._id === items
+                        );
+                        if (MyStudent) {
+                          return (
+                            <>
+                              <tr key={MyStudent._id}>
+                                <td>{++x}</td>
+                                <td
+                                  style={{
+                                    textAlign: "left",
+                                    paddingLeft: "1rem",
+                                  }}
+                                >
+                                  {MyStudent.name}
+                                </td>
+                                <td style={{ textTransform: "none" }}>
+                                  {MyStudent.email}
+                                </td>
+                                <td>
+                                  <button
+                                    className="rmvbtn"
+                                    onClick={async () => {
+                                      const UserId = MyStudent._id;
+                                      const ClassId = id;
+                                      console.log(UserId);
+                                      console.log(ClassId);
+                                      const res = await fetch(
+                                        "/removefromclass",
+                                        {
+                                          method: "POST",
+                                          headers: {
+                                            "content-Type": "application/json",
+                                          },
+                                          body: JSON.stringify({
+                                            UserId,
+                                            ClassId,
+                                          }),
+                                        }
+                                      );
+                                      if (res.status === 200) {
+                                        setLoading(true);
+                                        setTimeout(() => {
+                                          setLoading(false);
+                                        }, 5000);
+                                      } else {
+                                        console.log(res);
+                                      }
+                                    }}
+                                  >
+                                    Delete
+                                  </button>
+                                </td>
+                              </tr>
+                            </>
+                          );
+                        }
+                      })}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
+          </div>
         </>
       );
     }
@@ -301,17 +309,17 @@ console.log(MyClassroom);
   if (Loading) {
     return <Loader />;
   } else {
-    if (MyClassroom.length>0) {
+    if (MyClassroom.length > 0) {
       return (
         <>
-          <div className="add-course-header">
-            <Link to="/instructorDashboard/my-classroom">
-              <BiArrowBack className="backBtn" style={{ color: "white" }} />
-            </Link>
-          </div>
           <div
             className="my-class-main-container"
-            style={{ marginLeft: "6rem",marginRight:"2rem", paddingTop: "20px" }}>
+            style={{
+              marginLeft: "6rem",
+              marginRight: "2rem",
+              paddingTop: "20px",
+            }}
+          >
             <h1>My Classroom</h1>
             <div className="my-class-header">
               <div className="myclass-details">
@@ -331,8 +339,7 @@ console.log(MyClassroom);
                 </p>{" "}
                 <br />
                 <p>
-                  <strong>Class : </strong>{" "}
-                  {MyClassroom[0].Class || +"10"}th
+                  <strong>Class : </strong> {MyClassroom[0].Class || +"10"}th
                 </p>{" "}
                 <br />
                 <p>
@@ -351,7 +358,6 @@ console.log(MyClassroom);
                 </p>{" "}
                 <br />
               </div>
-              
             </div>
             <div className="manage-class-container">
               {/* The Linker Page to navigate the components  */}
@@ -362,26 +368,26 @@ console.log(MyClassroom);
                     onClick={handleParticipantShow}
                     className={ParticipantShow ? "bt-active" : ""}
                   >
-                   Add Participants
+                    Add Participants
                   </button>
                   <button
                     onClick={handleNoticeShow}
                     className={NoticeShow ? "bt-active" : ""}
                   >
-                   Add Activities
+                    Add Activities
                   </button>
                   <button
                     onClick={handleNotesShow}
                     className={NotesShow ? "bt-active" : ""}
                   >
-                   Add Notes
+                    Add Notes
                   </button>
 
                   <button
                     onClick={handleAtandanceShow}
                     className={AtandanceShow ? "bt-active" : ""}
                   >
-                  View Attendance
+                    View Attendance
                   </button>
                   <button
                     onClick={handleScheduleShow}
@@ -390,7 +396,6 @@ console.log(MyClassroom);
                     Schedule Class
                   </button>
                 </div>
-              
               </div>
 
               {/* Showing Participant if Participant is Active  */}
@@ -403,11 +408,22 @@ console.log(MyClassroom);
               {/* {NotesShow && <ClassNotes />} */}
 
               {/* Showing Atandance if Atandance is Active  */}
-              {AtandanceShow && <LiveClassAttendance MyClassroom={MyClassroom} StudentInfo={StudentInfo}/>}
+              {AtandanceShow && (
+                <LiveClassAttendance
+                  MyClassroom={MyClassroom}
+                  StudentInfo={StudentInfo}
+                />
+              )}
               {/* Showing ScheduleClassShow  if ScheduleClassShow  is Active  */}
-              {ScheduleClassShow && <SchedulseClass Instructor={instructorInfo} classId={id} MyClassroom={MyClassroom}  />}
+              {ScheduleClassShow && (
+                <SchedulseClass
+                  Instructor={instructorInfo}
+                  classId={id}
+                  MyClassroom={MyClassroom}
+                />
+              )}
             </div>
-           </div>
+          </div>
         </>
       );
     }
