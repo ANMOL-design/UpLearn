d5d330071c7c3d0ff16acd255c95b5da83c5b174
Aastha <aastha252001@gmail.com>
2022-08-25 17:44:49 +0530

    changes in home element

--- frontend/src/components/Home/StudentInstructorCard.jsx
+++ frontend/src/components/Home/StudentInstructorCard.jsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 
 export default function StudentInstructorCard() {
   let navigate = useNavigate();
-
   const handle1 = () => {
     navigate("/contact");
   };
@@ -14,31 +13,25 @@ export default function StudentInstructorCard() {
   };
   return (
     <div className="studInsCard">
-      <div className="studInsCardItem one">
+      <div onClick={handle1} className="studInsCardItem one">
         <div className="part1">
           <h3>Come Learn with UpLearn</h3>
           <p>
             Sign up today and get access to hundreds of resources, ebooks and
             video lectures free of cost
           </p>
-          <button type="button" onClick={handle1}>
-            Join
-          </button>
         </div>
         <div className="part2">
           <img src={studentpic} alt="" />
         </div>
       </div>
-      <div className="studInsCardItem two">
+      <div onClick={handle2} className="studInsCardItem two">
         <div className="part1">
           <h3>Become an Instructor</h3>
           <p>
             Top instructors from across the country are a part of the uplearn
             family today join now
           </p>
-          <button type="button" onClick={handle2}>
-            Join
-          </button>
         </div>
         <div className="part2">
           <img src={teacherpic} alt="" />
