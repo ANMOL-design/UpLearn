5a77a92882d60ac53c06b91b3c67848e9d343d4b
Aastha <aastha252001@gmail.com>
2022-08-25 16:39:06 +0530

    home page element

---
+++ frontend/src/components/Home/StudentInstructorCard.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import studentpic from "../../assets/images/student.png";
+import teacherpic from "../../assets/images/teacher.png";
+import { useNavigate } from "react-router-dom";
+
+export default function StudentInstructorCard() {
+  let navigate = useNavigate();
+
+  const handle1 = () => {
+    navigate("/contact");
+  };
+  const handle2 = () => {
+    navigate("/login");
+  };
+  return (
+    <div className="studInsCard">
+      <div className="studInsCardItem one">
+        <div className="part1">
+          <h3>Come Learn with UpLearn</h3>
+          <p>
+            Sign up today and get access to hundreds of resources, ebooks and
+            video lectures free of cost
+          </p>
+          <button type="button" onClick={handle1}>
+            Join
+          </button>
+        </div>
+        <div className="part2">
+          <img src={studentpic} alt="" />
+        </div>
+      </div>
+      <div className="studInsCardItem two">
+        <div className="part1">
+          <h3>Become an Instructor</h3>
+          <p>
+            Top instructors from across the country are a part of the uplearn
+            family today join now
+          </p>
+          <button type="button" onClick={handle2}>
+            Join
+          </button>
+        </div>
+        <div className="part2">
+          <img src={teacherpic} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
